fe11b43c7bba165c8c4f088c1a21a9f4bc3cd4d2
Scott <scrosehart@gmail.com>
2018-01-30 08:54:35 -0800

    added submit on enter button and updated button styling

--- js/script.js
+++ js/script.js
@@ -21,6 +21,17 @@ $(function() {
   $("#sortable").disableSelection();
 });
 
+$('.form').on('submit', function (e) {
+  e.preventDefault();
+  $('input:text:visible:first').focus();
+  var userInput = $('#userInput');
+  var userText = userInput.val();
+  var newItem = $('<li>');
+  newItem.html('<span class="arrow">&#8597;</span>' + userText + '<a>&times;<span class="delete">delete item</span></a>');
+  toDoList.append(newItem);
+  userInput.val('');
+});
+
 $(document).ready(function() {
   var button = $('#button');
   $('#button').on('click', addItem)
